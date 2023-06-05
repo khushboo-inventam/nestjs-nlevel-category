e742eb3691135d9345223063995c435a763173d0
Twinkle <twinkle@inventam.com>
2023-06-05 18:46:03 +0530

    Write a test case for  dynamic-col module .

--- src/dynamic-columns/dynamic-columns.controller.ts
+++ src/dynamic-columns/dynamic-columns.controller.ts
@@ -1,37 +1,45 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query, UseFilters, UsePipes, ValidationPipe } from '@nestjs/common';
 import { DynamicColumnsService } from './dynamic-columns.service';
 import { CreateDynamicColumnDto } from './dto/create-dynamic-column.dto';
 import { UpdateDynamicColumnDto } from './dto/update-dynamic-column.dto';
 import { ApiTags } from '@nestjs/swagger';
 import { SearchTracksDto } from '../common/SearchTracksDto.dto';
+import { AllExceptionsFilter } from '../common/all-exceptions.filter';
+import { MessagePattern } from '@nestjs/microservices';
 
 
+@UseFilters(new AllExceptionsFilter())
+@UsePipes(new ValidationPipe({ transform: true }))
 @ApiTags("dynamic-columns")
 @Controller('dynamic-columns')
 export class DynamicColumnsController {
-  constructor(private readonly dynamicColumnsService: DynamicColumnsService) {}
+  constructor(private readonly dynamicColumnsService: DynamicColumnsService) { }
 
-  @Post()
+  @MessagePattern("dynamic_col_create")
+  // @Post()
   create(@Body() createDynamicColumnDto: CreateDynamicColumnDto) {
     return this.dynamicColumnsService.create(createDynamicColumnDto);
   }
-
-  @Get()
+  @MessagePattern("dynamic_col_search_by_name")
+  // @Get()
   findAll(@Query() params?: SearchTracksDto) {
     return this.dynamicColumnsService.findAll(params);
   }
 
-  @Get(':id')
+  @MessagePattern("dynamic_col_search_by_dynamic_col_id")
+  // @Get(':id')
   findOne(@Param('id') id: string) {
     return this.dynamicColumnsService.findOne(+id);
   }
 
-  @Patch(':id')
+  @MessagePattern("dynamic_col_update_dynamic_col_by_id")
+  // @Patch(':id')
   update(@Param('id') id: string, @Body() updateDynamicColumnDto: UpdateDynamicColumnDto) {
     return this.dynamicColumnsService.update(+id, updateDynamicColumnDto);
   }
 
-  @Delete(':id')
+  @MessagePattern("dynamic_col_delete_by_dynamic_col_id")
+  // @Delete(':id')
   remove(@Param('id') id: string) {
     return this.dynamicColumnsService.remove(+id);
   }
---
+++ src/dynamic-columns/dynamic-columns.spec.ts
@@ -0,0 +1,124 @@
+import { Test, TestingModule } from "@nestjs/testing";
+
+import { HttpStatus, INestApplication, ValidationPipe } from "@nestjs/common";
+import { ClientProxy, ClientsModule, Transport } from "@nestjs/microservices";
+import { AppModule } from "../app.module";
+
+describe("DYNAMIC-COL", () => {
+  let app: INestApplication;
+
+  let client: ClientProxy;
+  let trueData: boolean;
+  let id: string;
+  let detailed: boolean;
+
+  beforeAll(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [
+        AppModule,
+        ClientsModule.register([
+          { name: "DYNAMIC-COL", transport: Transport.TCP },
+        ]),
+      ],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+
+    app.connectMicroservice({
+      transport: Transport.TCP,
+    });
+
+    app.useGlobalPipes(new ValidationPipe({ transform: true }));
+    await app.startAllMicroservices();
+    await app.init();
+
+    client = app.get("DYNAMIC-COL");
+    await client.connect();
+
+
+  });
+
+  afterAll(async () => {
+
+    await app.close();
+    client.close();
+    console.log("afterAll");
+  });
+
+  describe("dynamic-col-Module", () => {
+    describe("dynamic_col_create", () => {
+      it("Dynamic-col / (POST)", async () => {
+        try {
+          const response = await client
+            .send("dynamic_col_create", {
+              name: "",
+              type: ""
+            })
+            .toPromise();
+
+          // console.log('response', response, typeof response)
+          expect(response.statusCode).toBe(HttpStatus.BAD_REQUEST);
+        } catch (err) {
+          // console.log('err', err, typeof err)
+          expect(err.statusCode).toBe(HttpStatus.BAD_REQUEST);
+        }
+      });
+
+      it("Dynamic-col / (POST) without key", async () => {
+        try {
+          const response = await client.send("dynamic_col_create", {}).toPromise();
+
+          // console.log('response', response, typeof response)
+          expect(response.statusCode).toBe(HttpStatus.BAD_REQUEST);
+        } catch (err) {
+          // console.log('err', err, typeof err)
+          expect(err.statusCode).toBe(HttpStatus.BAD_REQUEST);
+        }
+      });
+
+      it("Dynamic-col / (POST) with wrong parent_Item_id", async () => {
+        try {
+          const response = await client
+            .send("dynamic_col_create", {
+              name: 5,
+            })
+            .toPromise();
+
+          expect(response.statusCode).toBe(HttpStatus.BAD_REQUEST);
+        } catch (err) {
+          expect(err.statusCode).toBe(HttpStatus.BAD_REQUEST);
+        }
+      });
+
+      // it("Item / (GET) find Item_search_by_Item_id", async () => {
+      //   try {
+      //     const response = await client
+      //       .send("item_search_by_item_id", +1)
+      //       .toPromise();
+      //     console.log('response.....***************.get  ', response.statusCode)
+      //     expect(response.statusCode).toBe(HttpStatus.NOT_FOUND);
+      //   } catch (err) {
+      //     expect(err.statusCode).toBe(HttpStatus.NOT_FOUND);
+      //   }
+      // });
+    });
+  });
+
+  //   describe('Get Item', () => {
+  //           it('/Item (GET)', async () => {
+  //             await request(app.getHttpServer()).get(`/Item/${id}/`).expect(HttpStatus.UNAUTHORIZED);
+  //           });
+  //           it('/Item (GET)', async () => {
+  //             await request(app.getHttpServer())
+  //               .get(`/Item/${trueData}/`)
+  //               .expect(HttpStatus.INTERNAL_SERVER_ERROR);
+  //           });
+  //           it('/Item (GET)', async () => {
+  //             detailed = false;
+  //             await request(app.getHttpServer())
+  //               .get(`/Item/?${detailed}/`)
+
+  //               .expect(HttpStatus.OK);
+  //           });
+  //         });
+});
